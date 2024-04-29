726e9353ee2e08c1eb4f95d1e9b366a2edc65fc3
GarciaBaptiste <baptiste1garcia@gmail.com>
2024-04-29 11:47:23 +0200

    rotate each face and the scene

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,28 @@ import './App.css'
 import React from 'react';
 import styled from 'styled-components'
 
-const LoadingWrapper = styled.section``;
+const LoadingWrapper = styled.section`
+`;
 
-const Scene = styled.div``;
+const Scene = styled.div`
+  transform-style: preserve-3d;
+  transform: rotateY(0deg);
+  position: relative;
+  animation: rotate 5s infinite reverse;
+`;
 
 const Face = styled.div`
   width: 100vw;
   height: 100vh;
   background: black;
+  transform: rotateY(${props => props.rotation}deg) translateZ(50vw);
+  position: absolute;
 `;
 
 const FaceBackground = styled.div`
   width: 100%;
   height: 100%;
-  border: solid 2px black;
+  border: solid 3px black;
   border-radius: 35vw;
   background: white;
   display: flex;
@@ -27,16 +35,14 @@ const LoadingText = styled.p`
   font-size: 24px;
 `;
 
-class BasicFace extends React.Component {
-  render() {
-    return(
-      <Face>
-        <FaceBackground>
-          <LoadingText><i>Loading...</i></LoadingText>
-        </FaceBackground>
-      </Face>
-    )
-  }
+const BasicFace = ({ rotation }) => {
+  return (
+    <Face rotation={rotation}>
+      <FaceBackground>
+        <LoadingText><i>Loading...</i></LoadingText>
+      </FaceBackground>
+    </Face>
+  )
 }
 
 function App() {
@@ -44,7 +50,10 @@ function App() {
     <>
       <LoadingWrapper>
         <Scene>
-          <BasicFace></BasicFace>
+          <BasicFace rotation={0}></BasicFace>
+          <BasicFace rotation={90}></BasicFace>
+          <BasicFace rotation={180}></BasicFace>
+          <BasicFace rotation={270}></BasicFace>
         </Scene>
       </LoadingWrapper>
     </>
