92c0840c4ebf54c8bc84837562c9e9932601c4d9
GarciaBaptiste <baptiste1garcia@gmail.com>
2024-04-29 10:26:20 +0200

    basic layout and components without any 3d

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
 import './App.css'
+import React from 'react';
+import styled from 'styled-components'
+
+const LoadingWrapper = styled.section``;
+
+const Scene = styled.div``;
+
+const Face = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: black;
+`;
+
+const FaceBackground = styled.div`
+  width: 100%;
+  height: 100%;
+  border: solid 2px black;
+  border-radius: 35vw;
+  background: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoadingText = styled.p`
+  font-size: 24px;
+`;
+
+class BasicFace extends React.Component {
+  render() {
+    return(
+      <Face>
+        <FaceBackground>
+          <LoadingText><i>Loading...</i></LoadingText>
+        </FaceBackground>
+      </Face>
+    )
+  }
+}
 
 function App() {
   return (
     <>
+      <LoadingWrapper>
+        <Scene>
+          <BasicFace></BasicFace>
+        </Scene>
+      </LoadingWrapper>
     </>
   )
 }
